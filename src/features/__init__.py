ce7fd3389be4f576a755af7236e34da521a4b662
sfruf <sebastianfruf@gmail.com>
2021-03-10 15:52:17 +0000

    Start setting up feature loading

---
+++ src/features/__init__.py
@@ -0,0 +1,29 @@
+
+def load_features(data_dir:str):
+    '''
+    Helper function to load all the features used in analysis
+
+    class_labels,two_class_labels,pos_str,neg_str,clus_co,ar_array,num_regions,num_subjs=load_features(data_dir)
+
+    Loads network and autoregressive features, labels, and calculates some useful shape parameters
+
+    '''
+    
+    import numpy as np
+
+    if data_dir[-1] is not '/': #check path format
+        data_dir.append('/')
+
+    class_labels=np.load(f'{data_dir}processed/Class_Labels.npy')
+    two_class_labels=np.load(f'{data_dir}processed/Two_Class_Labels.npy')
+    pos_str=np.load(f'{data_dir}processed/Pos.npy')
+    neg_str=np.load(f'{data_dir}processed/Neg.npy')
+    clus_co=np.load(f'{data_dir}processed/Clus.npy')
+    ar_array=np.load(f'{data_dir}processed/Autoreg.npy')
+
+    num_regions=ar_array.shape[0]
+    num_subjs=ar_array.shape[1]
+
+    return class_labels,two_class_labels,pos_str,neg_str,clus_co,ar_array,num_regions,num_subjs
+
+
